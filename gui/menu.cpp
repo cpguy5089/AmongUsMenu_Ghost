4bba5afc2c3d5247e2153743b193a9eeba82e97e
cpguy5089 <cpguy5089@users.noreply.github.com>
2023-03-24 12:15:09 +1100

    Make the GUI display a different name
    To show the difference between the OG and this edit

--- gui/menu.cpp
+++ gui/menu.cpp
@@ -30,7 +30,7 @@ namespace Menu {
 		if (!init)
 			Menu::Init();
 
-		ImGui::Begin("AmongUsMenu", &State.ShowMenu, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+		ImGui::Begin("AmongUsMenu_GHOST", &State.ShowMenu, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
 		ImGui::BeginTabBar("AmongUs#TopBar", ImGuiTabBarFlags_NoTabListScrollingButtons);
 
 		if(!firstRender)
