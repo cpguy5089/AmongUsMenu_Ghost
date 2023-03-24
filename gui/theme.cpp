309cbf8a5e3eb7be1bdfc82f7d851d728cce9302
cpguy5089 <cpguy5089@users.noreply.github.com>
2023-03-24 12:12:19 +1100

    Changing colour scheme
    Spicing it up a lil bit

--- gui/theme.cpp
+++ gui/theme.cpp
@@ -3,11 +3,11 @@
 #include "state.hpp"
 #include <imgui/imgui.h>
 
-#define HI(v)   ImVec4(0.502f, 0.075f, 0.256f, v)
-#define MED(v)  ImVec4(0.455f, 0.198f, 0.301f, v)
-#define LOW(v)  ImVec4(0.232f, 0.201f, 0.271f, v)
-#define BG(v)   ImVec4(0.200f, 0.220f, 0.270f, v)
-#define IMGUI_TEXT(v) ImVec4(0.860f, 0.930f, 0.890f, v)
+#define HI(v)   ImVec4(0.075f, 0.502f, 0.321f, v)
+#define MED(v)  ImVec4(0.198f, 0.455f, 0.352f, v)
+#define LOW(v)  ImVec4(0.240f, 0.271f, 0.201f, v)
+#define BG(v)   ImVec4(0.270f, 0.250f, 0.200f, v)
+#define IMGUI_TEXT(v) ImVec4(0.930f, 0.860f, 0.900f, v)
 
 void ApplyTheme()
 {
