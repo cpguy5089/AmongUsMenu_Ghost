3bea50f6d8aad8b3129cbd8e078d5b2ee669f382
cpguy5089 <cpguy5089@users.noreply.github.com>
2023-08-18 16:10:05 +1000

    Manually merge code from Dasupergrasskakjd
    HOPEFULLY THIS FIXES MY ISSUES WITH COMPILING NOW

--- rpc/CustomRpcHandler.cpp
+++ rpc/CustomRpcHandler.cpp
@@ -4,6 +4,7 @@
 #include "state.hpp"
 #include "logger.h"
 #include "utility.h"
+#define stringify(x) #x
 
 void HandleRpc(uint8_t callId, MessageReader* reader) {
 	switch (callId) {
@@ -13,9 +14,9 @@ void HandleRpc(uint8_t callId, MessageReader* reader) {
 		uint8_t playerid = MessageReader_ReadByte(reader, NULL);
 		if (!std::count(State.aumUsers.begin(), State.aumUsers.end(), playerid)) {
 			State.aumUsers.push_back(playerid);
-			GameData_PlayerInfo* playerinfo = GetPlayerDataById((Game::PlayerId)playerid); /* Popup on AUM user - Code from https://github.com/BitCrackers/AmongUsMenu/blob/0b38319a5efddffa4ef3e740f088fafe07052b88/rpc/CustomRpcHandler.cpp */
-			MessageBoxA(NULL, std::format("Received AUM player {}! Use AUM chat to talk with them!", app::GameData_PlayerInfo_get_PlayerName(playerinfo, NULL)).c_str(), "AUM player detected", MB_OK | MB_ICONEXCLAMATION);
-			STREAM_DEBUG("RPC Received for another AUM User from " << ToString((Game::PlayerId)playerid));
+			GameData_PlayerInfo* playerinfo = GetPlayerDataById((Game::PlayerId)playerid); /* Code ripped from Dasupergrasskakjd */
+			MessageBoxA(NULL, std::format("Received AUM player {}! Use AUM chat to talk with them!", stringify(app::GameData_PlayerInfo_get_PlayerName(playerinfo, NULL))).c_str(), "AUM player detected", MB_OK | MB_ICONEXCLAMATION);
+			STREAM_DEBUG("RPC Received for another AUM User from " << playerid);
 		}
 	}
 	break;
